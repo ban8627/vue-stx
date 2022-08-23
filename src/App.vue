9369d7f5bf141122bb3fa75fdd368be400875bf4
banssa <ban8627@gmail.com>
2022-08-23 10:50:45 +0900

    vue전환 초기

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,209 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 안내창 -->
+  <div class="modal-wrap" v-show="showModal" v-on:click.stop="closeModal" v-on:click.prevent>
+    <div class="modal-main">
+      <h1>STX 건설 클론코딩</h1>
+      <p>
+        이 사이트는 <b>스터디용</b>으로 제작되었으며 리소스는 <strong>원저작자</strong>에게 있습니다. <br>
+        해당 사이트 관련 문제 사항이 있으시면 <a href="mailto:aaa@aaa.net"><b>연락</b></a>해주시면 삭제하겠습니다.<br>
+        본 사이트는 Chrome 에 최적화되어 있습니다.
+      </p>
+      <button class="modal-close" v-on:click.stop="closeModal" v-on:click.prevent>
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
+  <div class="wrap">
+    <Header />
+    <Visual />
+    <Part />
+    <News :ntdata="noticeData" :nsdata="newsData" />
+    <Cs />
+    <About />
+    <Footer />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import {ref} from 'vue';
+  import Header from '@/components/HeaderView.vue';
+  import Visual from '@/components/VisualView.vue';
+  import Part from '@/components/PartView.vue';
+  import News from '@/components/NewsView.vue';
+  import Cs from '@/components/CsView.vue';
+  import About from '@/components/AboutView.vue';
+  import Footer from '@/components/FooterView.vue';
+  // import $ from 'jquery';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Visual,
+      Part,
+      News,
+      Cs,
+      About,
+      Footer
+    },
+    setup() {
+      const noticeData = [{
+        url: 'a.html',
+        title: '에스티엑스건설자산관리 주식회사 해산결의에 따른 채권신고 안내 공고 (2차)'
+      }, {
+        url: 'b.html',
+        title: '에스티엑스건설자산관리 주식회사 해산결의에 따른 채권신고 안내 공고 (1차)'
+      }, {
+        url: 'c.html',
+        title: '2021년도 협력업체 모집공고'
+      }, {
+        url: 'd.html',
+        title: 'STX건설 상호 사용 관련 안내'
+      }];
+
+      const newsData = [{
+        url: 'a.html',
+        title: 'STX건설, 춘천 레고랜드 테마파크 시공사 ‘선정’'
+      }, {
+        url: 'b.html',
+        title: 'STX건설, 2018년 성장 "청신호"'
+      }];
+
+      let showModal = ref(true);
+
+      const closeModal = ()=> {
+        // 모달 창 닫기 변수
+        showModal.value = false;
+      }
+
+      return {
+        noticeData,
+        newsData,
+        showModal,
+        closeModal,
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @charset "utf-8";
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+
+  img {
+    vertical-align: middle;
+    border: 0;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', Helvetica, '맑은 고딕', 'malgun gothic', 'Apple SD Gothic Neo', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 1.4;
+    letter-spacing: -0.64px;
+    color: #444;
+  }
+
+  /* 공통요소 */
+  .clearfix::after {
+    content: '';
+    position: relative;
+    display: block;
+    width: 100%;
+    clear: both;
+  }
+
+  .inner {
+    position: relative;
+    display: block;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+/* 안내창 */
+.modal-wrap {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  z-index: 99999;
 }
+.modal-main {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 550px;
+  height: 400px;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 10px;
+  box-shadow: 0px 0px 15px 0px rgb(0 0 0 / 70%);
+  border: 5px solid rgba(0, 0, 0, 0.1);
+}
+
+.modal-main h1 {
+  font-size: 30px;
+  font-weight: 600;
+  text-align: center;
+  color: #000;
+  padding: 20px 0;
+}
+.modal-main p {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 300;
+  color: #111;
+  line-height: 1.8;
+}
+
+.modal-main b {
+  font-weight: 600;
+  text-decoration: underline;
+  color: #ed1c24;
+}
+.modal-main strong {
+  font-weight: 600;
+  text-decoration: underline;
+  color: #ed1c24;
+}
+.modal-close {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  border: 0;
+  cursor: pointer;
+  font-size: 24px;
+  background: transparent;
+  color: #999;
+  transition: transform 0.3s;
+}
+
+.modal-close:hover {
+  transform: rotate(90deg);
+}
+  .wrap {
+    padding-top: 80px;
+  }
 </style>
---
+++ src/components/HeaderView.vue
@@ -0,0 +1,207 @@
+<template>
+      <!-- 상단 -->
+    <header class="header">
+      <div class="inner">
+        <a href="#" class="logo" >stx건설</a>
+        <nav class="gnb">
+          <ul class="depth1">
+            <li>
+              <a href="#">회사소개</a>
+              <ul class="depth2">
+                <li><a href="#">인사말</a></li>
+                <li><a href="#">STX건설</a></li>
+                <li><a href="#">기업문화</a></li>
+                <li><a href="#">STX건설연혁</a></li>
+                <li><a href="#">조직안내</a></li>
+                <li><a href="#">윤리경영</a></li>
+                <li><a href="#">안전·환경·품질경영</a></li>
+                <li><a href="#">찾아오시는길</a></li>
+                
+              </ul>
+            </li>
+            <li>
+              <a href="#">사업분야</a>
+              <ul class="depth2">
+                <li><a href="#">건축사업</a></li>
+                <li><a href="#">주택사업</a></li>
+                <li><a href="#">토목사업</a></li>
+                <li><a href="#">플랜트/공작기계사업</a></li>
+                <li><a href="#">해외사업</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">사회공헌</a>
+              <ul class="depth2">
+                <li><a href="#">나눔의생각</a></li>
+                <li><a href="#">주요활동분야</a></li>
+                <li><a href="#">활동현황</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">홍보센터</a>
+              <ul class="depth2">
+                <li><a href="#">홍보동영상</a></li>
+                <li><a href="#">홍보브로슈어</a></li>
+                <li><a href="#">STX건설뉴스</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">고객지원</a>
+              <ul class="depth2">
+                <li><a href="#">자주묻는질문</a></li>
+                <li><a href="#">고객문의</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">채용정보</a>
+              <ul class="depth2">
+                <li><a href="#">채용안내</a></li>
+                <li><a href="#">채용공고</a></li>
+                <li><a href="#">채용FAQ</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div class="lang">
+          <a href="#">ENG</a>
+          <i class="lang-bar"></i>
+          <a href="#" class="lang-active">KOR</a>
+        </div>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+/* 상단 */
+.header {
+  position: fixed;
+  left: 0;
+  top: 0;
+  overflow: hidden;
+  width: 100%;
+  height: 80px;
+  transition: height 0.5s;
+  z-index: 9999;  
+  box-shadow: 0px 0px 7px 0px rgb(0 0 0 / 20%);
+}
+
+.header-open {
+  height: 400px;
+}
+
+.header::before {
+  content: '';
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 0;
+  width: 100vw;
+  height: 80px;
+  background: #fff;
+  box-shadow: 0px 0px 7px 0px rgb(0 0 0 / 20%);
+}
+
+.header .inner {}
+
+.logo {
+  position: absolute;
+  right: calc(100% + 100px);
+  top: 20px;
+  width: 150px;
+  height: 39px;
+  background: url('../assets/images/logo.png') no-repeat center;
+  background-size: cover;
+  font-size: 0;
+}
+
+.gnb {
+  position: relative;
+}
+
+.gnb::before {
+  content: '';
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 80px;
+  display: block;
+  width: 100vw;
+  height: calc(100% - 80px);
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.depth1 {
+  position: relative;
+  display: table;
+  table-layout: fixed;
+  width: calc(180px * 6 - 70px);
+  margin: 0 auto;
+}
+
+.depth1>li {
+  display: table-cell;
+  width: 180px;
+}
+
+.depth1>li:last-child {
+  width: 70px;
+}
+
+.depth1>li>a {
+  position: relative;
+  font-size: 19px;
+  color: #000;
+  font-weight: 500;
+  line-height: 80px;
+}
+
+.depth1-focus {
+  color: #ed1c24 !important;
+}
+
+.depth2 {
+  padding-top: 40px;
+  padding-bottom: 20px;
+}
+
+.depth2 li a {
+  display: inline-block;
+  font-size: 15px;
+  color: #fff;
+  margin-bottom: 15px;
+}
+
+.lang {
+  position: absolute;
+  left: calc(100% + 100px);
+  top: 26px;
+  font-size: 0;
+  white-space: nowrap;
+}
+
+.lang a {
+  display: inline-block;
+  font-size: 15px;
+  color: #888;
+  font-weight: 500;
+  text-transform: uppercase;
+}
+
+.lang .lang-active {
+  color: #000;
+}
+
+.lang-bar {
+  display: inline-block;
+  width: 1px;
+  height: 9px;
+  background-color: #999;
+  margin: 0 10px;
+}
+</style>
